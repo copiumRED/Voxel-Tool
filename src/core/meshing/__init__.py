46e2c0fda5b65233fd3dc0e8cc7f19c7146f95db
copiumRED <copiumred@gmail.com>
2026-02-28 22:53:11 +0200

    Revert "Roadmap 12: greedy meshing for solidify"
    This reverts commit 6878b38d5e5520f7f4ad34e39c905a8a7131f02c.

--- src/core/meshing/__init__.py
+++ src/core/meshing/__init__.py
@@ -1,6 +1,4 @@
 from core.meshing.mesh import SurfaceMesh
-from core.meshing.greedy_mesher import extract_greedy_surface_mesh
-from core.meshing.solidify import build_solid_mesh
 from core.meshing.surface_extractor import extract_surface_mesh
 
-__all__ = ["SurfaceMesh", "extract_surface_mesh", "extract_greedy_surface_mesh", "build_solid_mesh"]
+__all__ = ["SurfaceMesh", "extract_surface_mesh"]
